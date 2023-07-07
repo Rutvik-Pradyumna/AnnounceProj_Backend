089e9e893588fc515bd12565dc05e146503edb29
Sohan <sreecharansohanoffl@gmail.com>
2023-07-07 15:23:26 +0530

    user model update

--- models/user.js
+++ models/user.js
@@ -25,7 +25,11 @@ const userSchema =new mongoose.Schema({
     password:{
         type:String,
         required:true
-    }
+    },
+    posts:[{
+        type:Schema.Types.ObjectId,
+        ref:'Post'
+    }]
 });
 
 
