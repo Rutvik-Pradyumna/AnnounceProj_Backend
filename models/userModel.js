a5d4de8eecc453d102f11c00ad965838d5b53b46
Rutvik Pradyumna <pradyumnarutvik@gmail.com>
2023-07-20 21:29:12 +0530

    Added User Verification

--- models/userModel.js
+++ models/userModel.js
@@ -29,6 +29,10 @@ const userSchema =new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:'Post'
     }],
+    isVerified:{
+        type:Boolean,
+        default:false
+    },
     tokens:[{
         type:Object
     }]
